5bd44f602079dc7c7b3da495b7978e9eeba31b00
Kallebe18 <kallebegomes18@gmail.com>
2020-04-29 22:50:30 -0300

    Refactoring first migration

---
+++ src/database/migrations/20200430013736-create-types.js
@@ -0,0 +1,43 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('types', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.STRING,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+      },
+      sucategories_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      sucategories_categories_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      sucategories_categories_departments_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('types');
+  },
+};
