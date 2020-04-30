f37cdc3fef05798fbe18250e5b525462b92bbb4c
Kallebe18 <kallebegomes18@gmail.com>
2020-04-30 00:00:48 -0300

    Fixing time type

--- src/database/migrations/20200430015152-create-service-level-agreements.js
+++ src/database/migrations/20200430015152-create-service-level-agreements.js
@@ -10,7 +10,7 @@ module.exports = {
         autoIncrement: true,
       },
       time: {
-        type: Sequelize.INTEGER,
+        type: Sequelize.TIME,
         allowNull: false,
       },
       priority: {
